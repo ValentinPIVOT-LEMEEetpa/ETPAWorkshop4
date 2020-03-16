24ad13c57573b97fa4acffd94b6c91dd69fed26d
ValentinPIVOT-LEMEEetpa <pivot.valentin@laposte.net>
2020-03-16 17:29:08 +0100

    12

---
+++ main.js
@@ -0,0 +1,23 @@
+var config = {
+	type: Phaser.AUTO,
+	width: 1024,
+	height: 768,
+physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: x },
+            debug: true
+        }
+    },
+scene: [Scene0, Scene1, Scene2, Scene3]
+};
+
+function preload(){
+
+};
+function create(){
+
+};
+function update(){
+
+};
